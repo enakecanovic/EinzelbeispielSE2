b4840cd3f8468412b65ede8096fc29f0dd70dd3e
enakecanovic <enkecanovic@edu.aau.at>
2025-03-13 19:20:38 +0100

    sorting_by_time_adapted

--- src/main/kotlin/at/aau/serg/controllers/LeaderboardController.kt
+++ src/main/kotlin/at/aau/serg/controllers/LeaderboardController.kt
@@ -17,9 +17,12 @@ class LeaderboardController(
         //gameResultService.getGameResults().sortedWith(compareBy({ -it.score }, { it.id }))
 
         // new sorting_by_time
-        gameResultService.getGameResults().sortedWith(compareBy({ -it.score }, { it.timeInSeconds }))
+       //gameResultService.getGameResults().sortedWith(compareBy({ -it.score }, { it.timeInSeconds }))
 
-        // new sorting_by_time_2
-        //gameResultService.getGameResults().sortedWith(compareBy({ it.timeInSeconds }, { -it.score }))
+        //new sorting_by_time_2
+        //I have been familiarizing myself with the Kotlin syntax and concluded that "-" in "-it.score" represents descending order, so I have figured to try
+        //and challenge myself to find alternative for this and I did it with "byDescending" getting basically the same thing
+        //I have also figured even though it passed the tests, the 2nd solution has not been working correctly as it was 1st sorting by time and only in case times are equal it would sort them by score which is incorrect
+        gameResultService.getGameResults().sortedWith(compareByDescending<GameResult>{ it.score }.thenBy{ it.timeInSeconds})
 
 }
